627a30ea09942e9b133f2c53280be6626298d518
Aleksandra Sandanova <slowpoche@gmail.com>
2022-06-15 17:48:26 +0800

    Добавлена Лабораторная работа 1

---
+++ Lab1/TestCommand.py
@@ -0,0 +1,7 @@
+import math
+help(math)
+print(dir(math))
+help(math.sqrt)
+list1 = [82,8,23,97,92]
+dir(list1)
+help(list1.insert)
---
+++ Lab1/test.py
@@ -0,0 +1,5 @@
+import time
+delay = 3
+a = int(input())
+print(a**2)
+time.sleep(delay)
